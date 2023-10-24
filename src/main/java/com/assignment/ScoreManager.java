0c589a8125951fa8e4287638566da92a1e9a509f
meet-patel22 <meetpatidar1998@gmail.com>
2023-10-23 23:24:48 -0400

    first commmiteee

---
+++ src/main/java/com/assignment/ScoreManager.java
@@ -0,0 +1,14 @@
+package com.assignment;
+
+public class ScoreManager {
+
+	
+	public boolean checkPassingScore( int score ) {
+		
+		if( score >= 6 )
+			return true ;
+		else 
+			return false ;
+	}
+
+}
